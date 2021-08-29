ce6450a0dba35c24147289f382413c52f0ec010a
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-08-29 19:03:13 +0900

    feat: add heap

--- algorithm/heap.go
+++ algorithm/heap.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Heap []int
 type IHeap interface {
 	root() int
@@ -38,23 +40,51 @@ func isHeap(heap Heap, i int) bool {
 あるノードとその配下のノードがヒープの条件を満たすようにする操作
 1.あるノードとその子ノードはヒープの条件を満たさない
 2.子ノードとその配下のノードがヒープの条件を満たす時
+1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+-----------------------------
+1 |       |               |
 **/
 func min_heapify(heap Heap, i int) Heap {
-	left := 2*i + 1
-	right := 2*i + 2
+	left_child := 2*i + 1
+	right_child := 2*i + 2
 	length := len(heap) - 1
 	smallest := i
 
-	if left <= length && heap[i] > heap[left] {
-		smallest = left
+	// left_childがlengthより大きかったら => 葉なので停止
+	// heap[smallest]がheap[left_child]より小さかったら、最小ノードを変更
+	if left_child <= length && heap[smallest] > heap[left_child] {
+		smallest = left_child
 	}
-	if right <= length && heap[smallest] > heap[right] {
-		smallest = right
+	// right_childがlengthより大きかったら => 葉なので停止
+	// heap[left_child]がheap[right_child]より大きかったら、最小ノードを変更
+	if right_child <= length && heap[left_child] > heap[right_child] {
+		smallest = right_child
 	}
+	// 最小ノードがiと同じでなかったら変更があるので、smallestとiの値を交換
 	if smallest != i {
-		heap[i] = heap[smallest]
-		heap[smallest] = heap[i]
+		// 交換処理
+		current_node := heap[i]
+		smallest_node := heap[smallest]
+		heap[i] = smallest_node
+		heap[smallest] = current_node
+
+		fmt.Printf("%d番目の%dと%d番目の%dが変わって、%dが出た。\n", i, current_node, smallest, smallest_node, heap)
+
+		// smallestの場所から再帰処理
 		min_heapify(heap, smallest)
 	}
 	return heap
+}
+
+func build_min_heap(heap Heap) {
+	for i := len(heap) / 2; i >= 0; i-- {
+		min_heapify(heap, i)
+	}
+}
+
+func main() {
+	array := []int{4, 5, 6, 3, 2, 1, 9, 12, 8, -1, -2, 1, 4, 10, 5}
+	fmt.Printf("%d\n", array)
+	build_min_heap(array)
+	fmt.Printf("%d\n", array)
 }
---
+++ algorithm/heap_test.go
@@ -0,0 +1,6 @@
+package heap
+package algorithm
+func TestHeap() {
+	array := []int{4, 5, 6, 3, 2, 1}
+	algorithm.min
+}
