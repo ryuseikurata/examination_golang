9bebcca30aa1c679a9eabb8c76044216133ee252
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-09-01 13:29:03 +0900

    feat: add delete

--- algorithm/binary_search.go
+++ algorithm/binary_search.go
@@ -24,40 +24,25 @@ func (n *Node) insert(v int) {
 	}
 }
 
-// 再起的に降りて、目的の場所を見つけてノードを咲くおjする。削除した後にはdeterminの結果を埋め込む
+// 再起的に降りて、目的の場所を見つけてノードを咲くする。削除した後にはdeterminの結果を埋め込む
 func (n *Node) delete(v int) *Node {
-	for n != nil {
+	if n != nil {
 		if n.value == v {
-			// 削除ノードに子がない時
-			if n.left == nil && n.right == nil {
-				// 削除
-				n = nil
-				return n
-			} else if n.left != nil && n.right == nil {
-				// 削除ノードが一つの時(leftが存在) => leftを移動
-				n = n.left
-				return n
-			} else if n.left == nil && n.right != nil {
-				// 削除ノードが一つの時(rightが存在) => rightを移動
-				n = n.right
-				return n
+			if n.left == nil {
+				return n.right
+			} else if n.right == nil {
+				return n.left
 			} else {
-				// 削除ノードが二つの時
-				//　左部分木の最大値 or 右部分木の最小値を移動
-				// 今回は右部分木の最小値を移動させる
-				// 1. 最小値があるnode取得
-				// 2.
-				tmp := n.right.search_min()
-				n.value = tmp.value
-				n.right = n.right.delete(tmp.value)
-				return n
+				n.value = *&n.right.search_min().value
+				n.right = n.right.delete_min()
 			}
-
 		} else if n.value < v {
-			return n.right.delete(v)
+			n.right = n.right.delete(v)
 		} else {
-			return n.left.delete(v)
+			n.left = n.left.delete(v)
 		}
+	} else {
+		return n
 	}
 	return n
 }
@@ -122,6 +107,7 @@ func main() {
 			value: 19, left: &Node{
 				value: 17, left: nil, right: nil},
 			right: nil}}
+	fmt.Println(node)
 	node.delete(9)
 	fmt.Println(node)
 }
